9cef8c7f039e1bcefd66ec9f9ed4556c125a9886
Akalankagunathilake2002 <akalankaharshana2002@gmail.com>
2026-02-27 20:01:55 +0530

    Change the deploy URL of render

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5001/api";
+export const API_BASE_URL = "https://household-sustainability-system.onrender.com/api";
 
 export const API_ENDPOINTS = {
     AUTH: {
